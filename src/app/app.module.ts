54af87dd2d5a3b801a05f7356d6d597a4018daed
Roy Nevo <roy@webiks.com>
2018-12-27 09:18:21 +0200

    effects error. will look at it asap.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoService } from './services/todo.service';
+import {FormsModule} from '@angular/forms';
+import { TodoListComponent } from './components/todo/todo-list/todo-list.component';
+import { EffectsModule } from '@ngrx/effects';
+import { TodoListItemComponent } from './components/todo/todo-list-item/todo-list-item.component';
+import * as TodoReducer from './store/todo/todo.reducer';
+import {StoreModule} from '@ngrx/store';
+import { TodoDetailsComponent } from './components/todo/todo-details/todo-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent,
+    TodoListItemComponent,
+    TodoDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FormsModule,
+    StoreModule.forRoot({todos: TodoReducer.TodoReducer}),
+    EffectsModule.forRoot([TodoEffects])
   ],
-  providers: [],
+  providers: [
+    TodoService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
